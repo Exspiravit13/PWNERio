33fcc664221f8e8667f67876db0f3b01f36f4bdc
hajertamas <76472742+hajertamas@users.noreply.github.com>
2022-06-21 17:48:18 +0200

    + metadata & layout stuff

--- classes/Page.js
+++ classes/Page.js
@@ -14,20 +14,26 @@ export default class {
 
     _renderFn = (html)=>{ return new Promise((resolve, reject)=>{ resolve(html); }); };
 
-    constructor(identifier, html = null){
+    constructor(identifier){
+        //This should be the name of the folder and the html file too
         this._id = identifier
-        if(html == null){
-            html = this.id + ".html";
-        }
+        this._htmlfile = this.id + ".html";
+    }
 
-        this._htmlfile = html;
-
-        fs.readFile(`./templates/${this._id}/${this._htmlfile}`, (err, data) => {
-            if(err){
-                this._htmlstring = "Server error";
-                return;
-            }
-            this._htmlstring = data.toString();
+    load(){
+        /**
+            We make a promise out of the fs readFile callback so we can async/await or use Promise.then
+            Load all possibly heavy duty tasks related to page here
+        */
+        return new Promise((resolve, reject) => {
+            fs.readFile(`./templates/${this._id}/${this._htmlfile}`, (err, data) => {
+                if(err){
+                    this._htmlstring = "";
+                    return;
+                }
+                this._htmlstring = data.toString();
+                resolve();
+            });
         });
     }
 
@@ -43,6 +49,11 @@ export default class {
         this._layout = v;
     }
 
+    
+    set metadata(v){
+        this._metadata = v;
+    }
+
     _preRender(){
 
     }
@@ -50,41 +61,54 @@ export default class {
     render(){
         return new Promise(async (resolve, reject)=>{
             let html = "";
+            /* We do the render function which should be defined at implementation of the class */
             const rendered = await this._renderFn(this._htmlstring);
             try{
+                /* Check if we have a layout, and a valid one */
                 if(this._layout){
                     fs.readFile(`./layouts/${this._layout}.html`, (err, data) => {
                         if(err){ throw err; }
                         html += data.toString();
                         html = html.replace('{%layout_content%}', rendered);
+
+                        /* We insert our metadata into our layout (this is where <head> tag and nav should be i guess) */
+                        Object.keys(this._metadata).forEach((key) => {
+                            const value = this._metadata[key];
+                            html = html.replace(`{%meta.${key}%}`, value);
+                        });
+
                         resolve(html)
                     });
+                }else{
+                    resolve(html);
                 }
             }catch(e){
-                html = html.replace('{%layout_content%}', rendered);
-                resolve(html);
+                /* Just give back the plain rendered html if for some reason our layout couldn't load */
+                resolve(rendered);
             }
         });
     }
 
-    static insertData(to = "", what = {}){
-        let string = to;
-        const data = what;
-
+    static insertData(string = "", data = {}){
+        /* Get every instance where we possibly want to insert variables */
         const regexp = new RegExp('({%.*%})');
-        const matches = regexp.exec(to);
+        const matches = regexp.exec(string);
         if(matches == null) return;
         matches.forEach((match) => {
             try{
+                /*
+                    We match the whole thing because javascript regexp is weird and returns duplicate stuff that were not in a capturing group
+                    So we have to replace the {%...%}
+                */
                 const keys = match.replace('{%', '').replace('%}', '').split(".");
                 let currentValue = data;
-                let keysString = "";
+                let keysString = "";            //This is just for having a little bit more informative error messages
                 keys.forEach((key, depth) => {
+                    /* We go through our input object ($data) so we can have nested object as input */
                     keysString += "." + key;
                     if(!currentValue || !currentValue[key]){ throw new Error("Could not insert data because value with key \"" + keysString.substring(1) + "\" was not found"); }
                     currentValue = currentValue[key];
                 });
-
                 string = string.replace(match, currentValue);
             }catch(e){
                 console.log(e);
@@ -93,6 +117,5 @@ export default class {
         });
         return string;
     }
-
 
 }
--- templates/home/home.js
+++ templates/home/home.js
@@ -1,18 +1,28 @@
 import Page from "../../classes/Page.js"
 
 const page = new Page("home");
+
 page.layout = "main";
+
+page.metadata = {
+    title: "PWNER.IO",
+    description: "PWNER.IO is the latest up and coming cyber marketplace for all your pwning needs!",
+    image: "/static/assets/logo.png"
+}
 
 page.renderFn = function(html){
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            //get sum data etc
-            const data = {
-                something: "yes"
-            };
-            html = Page.insertData(html, data);
-            resolve(html);
-        }, 241);
+        page.load().then(()=>{
+            setTimeout(() => {
+                //get sum data etc
+                const data = {
+                    something: "yes"
+                };
+
+                html = Page.insertData(html, data);
+                resolve(html);
+            }, 241);
+        });
     });
 }
 
